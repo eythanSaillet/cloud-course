fda754c7d22546fffee84cdf6c708ec2b3cc3b0c
eythanSaillet <eythan.saillet@gmail.com>
2020-12-09 02:33:35 +0100

    :sparkles: linking postgres database

--- index.js
+++ index.js
@@ -2,17 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+const knex = require('knex')({
+	client: 'pg',
+	connection: {
+		host: 'localhost',
+		port: 5432,
+		user: 'test',
+		password: 'test',
+		database: 'postgres',
+		// ssl: { rejectUnauthorized: false },
+	},
 })
 
-app.get('/test', (req, res) => {
-	// res.send('Hello World!')
-	return res.status(200).json({
-		hello: 'world',
-	})
+app.get('/', async function (req, res) {
+	res.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', async function (req, res) {
+	return knex
+		.select('*')
+		.from('users')
+		.then((rows) => {
+			// console.log(rows)
+			return res.status(200).json(req)
+		})
+		.catch((err) => {
+			// console.log(err)
+			return res.status(400).json(err)
+		})
 })
 
 app.listen(port, () => {
-	console.log(`App listening at http://localhost:${port}`)
+	console.log(`App running on port ${port}.`)
 })
